be37e4b24c0323e3ccddd7732a79c0cfefba1cee
Grom679 <57178536+Grom679@users.noreply.github.com>
2025-01-20 22:06:14 +0200

    Finished quota generator

--- Assets/App/QuotaSystem/Scripts/Managers/QuotaManager.cs
+++ Assets/App/QuotaSystem/Scripts/Managers/QuotaManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,37 +7,110 @@ namespace DeepGame.Quota
 {
     public class QuotaManager : MonoBehaviour
     {
+        public Action OnQuotaFailed { get; set; }
+        public Action<float> OnNewDayGenerated { get; set; }
+        public Action OnQuotaCompleted { get; set; }
+
         [SerializeField]
         private Quota _startingQuota;
-
+        [SerializeField]
         private Quota _currentQuota;
+        [SerializeField]
+        private float _quotaMinPer = 0.2f;
+        [SerializeField]
+        private float _quotaMaxPer = 0.3f;
+        [SerializeField]
+        private float _quotaMaxDays = 6f;
+        [SerializeField]
+        private float _quotaDayMultiplier = 1.7f;
+
+        private int _currentDay = 1;
+        private float _currentQuotaValue = 0f;
+        private float _previousQuotaValue = 0f;
 
         public void InitializeQuota()
         {
             _currentQuota = GenerateQuota();
+            _previousQuotaValue = _currentQuota.quotaValue;
         }
 
-        public void FinishDay()
+        public Quota GetQuotaData()
         {
+            return _currentQuota;
+        }
 
+        public void FinishDay(float quotaDelta)
+        {
+            if(_currentDay == _currentQuota.daysCount)
+            {
+                if(_currentQuotaValue >= _currentQuota.quotaValue)
+                {
+                    ResetParameters();
+                    FinishQuota();
+                    OnQuotaCompleted?.Invoke();
+                }
+                else
+                {
+                    ResetCurrentValues();
+                    OnQuotaFailed?.Invoke();
+                }
+            }
+            else
+            {
+                _currentDay++;
+                _currentQuotaValue += quotaDelta;
+                OnNewDayGenerated?.Invoke(_currentQuota.quotaValue);
+            }
         }
 
         private void FinishQuota()
         {
+            _currentQuota = GenerateQuota();
+        }
 
+        private void ResetCurrentValues()
+        {
+            ResetParameters();
+            _currentQuota.quotaValue = 0f;
+            InitializeQuota();
+        }
+
+        private void ResetParameters()
+        {
+            _currentDay = 1;
+            _currentQuotaValue = 0f;
         }
 
         private Quota GenerateQuota()
         {
             Quota quota = null;
 
-            if (_currentQuota == null)
+            if (_currentQuota.quotaValue == 0f)
             {
                 quota = _startingQuota;
             }
             else
             {
                 quota = new Quota();
+                float increasePercentage = UnityEngine.Random.Range(_quotaMinPer, _quotaMaxPer);
+                float newQuotaValue = _currentQuota.quotaValue * (1 + increasePercentage);
+                int newDaysCount = _currentQuota.daysCount;
+
+                if (newQuotaValue >= _previousQuotaValue * _quotaDayMultiplier)
+                {
+                    if(newDaysCount >= _quotaMaxDays)
+                    {
+                        _previousQuotaValue = _currentQuota.quotaValue;
+                    }
+                    else
+                    {
+                        newDaysCount += 1;
+                        _previousQuotaValue = _currentQuota.quotaValue;
+                    }
+                }
+
+                quota.quotaValue = Mathf.Round(newQuotaValue);
+                quota.daysCount = newDaysCount;
             }
 
             return quota;
